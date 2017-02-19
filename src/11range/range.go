46e54de5d2d432e7cc30025c715c6b524b5b1e98
Li <li3zhang@gmail.com>
2017-02-19 15:02:40 -0500

    Closures

---
+++ src/11range/range.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  nums := []int{2,3,4}
+  sum := 0
+  for _, num := range nums {
+    sum += num
+  }
+  fmt.Println("sum: ", sum)
+
+  kvs := map[string]string{"a":"apple", "b":"banana"}
+  for k,v := range kvs {
+    fmt.Printf("key=%s val=%s\n", k, v)
+  }
+
+  for k := range kvs {
+    fmt.Println("key: ", k)
+  }
+
+  for i, c := range "go" {
+    fmt.Println(i, c)
+  }
+}
